4a7bb43329982ec07bca102e71db2261ff4c91e6
Alena59 <asdalena59@gmail.com>
2023-04-21 15:19:44 +0300

    no-swiper

---
+++ source/js/modules/swiper-trainers.js
@@ -0,0 +1,51 @@
+import Swiper from 'swiper';
+
+// import Swiper, {Navigation} from 'swiper';
+
+const initTrainersSwiper = (swiper) => {
+  if (swiper) {
+    // eslint-disable-next-line
+    new Swiper('swiper', {
+      modules: [Navigation],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      loop: true,
+
+      breakpoints: {
+
+        280: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          spaceBetween: 22,
+          initialSlide: 2,
+        },
+
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          spaceBetween: 22,
+          initialSlide: 2,
+        },
+
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 1,
+          spaceBetween: 30,
+          initialSlide: 2,
+        },
+
+        1200: {
+          slidesPerView: 4,
+          slidesPerGroup: 1,
+          spaceBetween: 40,
+          initialSlide: 0,
+        },
+      },
+    });
+  }
+};
+
+export {initTrainersSwiper};
