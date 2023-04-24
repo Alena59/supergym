f3a4fd80b3a7f5d24d080d65c4a5f48e8de6c424
Alena59 <asdalena59@gmail.com>
2023-04-24 03:29:43 +0300

    video

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -1,6 +1,6 @@
 function setupVideo() {
   const video = document.querySelector('.video');
-  // const link = video.querySelector('.video__link');
+  const picture = video.querySelector('.video picture');
   const button = video.querySelector('.video__btn');
 
   // button.removeAttribute('href');
@@ -9,7 +9,7 @@ function setupVideo() {
   button.addEventListener('click', () => {
     const iframe = createIframe();
 
-    // link.remove();
+    picture.remove();
     button.remove();
     video.appendChild(iframe);
   });
@@ -18,11 +18,12 @@ function setupVideo() {
 function createIframe() {
   let iframe = document.createElement('iframe');
 
-  iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?rel=0&showinfo=0&autoplay=1');
-  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
+  iframe.setAttribute('allowfullscreen', 'allowfullscreen');
   iframe.setAttribute('allow', 'autoplay');
-  iframe.classList.add('video__poster');
+  iframe.setAttribute('frameborder', '0');
   iframe.style.zIndex = '10';
+  // https://www.youtube.com/watch?v=9TZXsZItgdw
 
   return iframe;
 }
