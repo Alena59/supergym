3d4912ac80350e90de4aacef563d7529f208a8d8
Alena59 <asdalena59@gmail.com>
2023-04-26 02:45:39 +0300

    promo

--- source/js/modules/swiper-trainers.js
+++ source/js/modules/swiper-trainers.js
@@ -1,33 +1,36 @@
 import Swiper from '../vendor/swiper';
+const trainersSwiper = document.querySelector('.trainers__swiper');
 
 const initTrainersSwiper = () => {
-  new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
+  if (trainersSwiper) {
+    new Swiper('.trainers__swiper', {
+      direction: 'horizontal',
+      loop: true,
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    breakpoints: {
-
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
 
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-      },
+      breakpoints: {
 
-      1200: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
-    }
-  })
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      }
+    })
+  }
 }
 
 export {initTrainersSwiper};
