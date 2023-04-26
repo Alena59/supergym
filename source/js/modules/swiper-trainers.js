0d49ba5266277ccac26f1ddc8f97e1aa2431ba33
Alena59 <asdalena59@gmail.com>
2023-04-26 23:51:47 +0300

    trainers

--- source/js/modules/swiper-trainers.js
+++ source/js/modules/swiper-trainers.js
@@ -16,12 +16,12 @@ const initTrainersSwiper = () => {
 
         320: {
           slidesPerView: 1,
-          spaceBetween: 20,
+          // spaceBetween: 20,
         },
 
         768: {
           slidesPerView: 2,
-          spaceBetween: 30,
+          spaceBetween: 30
         },
 
         1200: {
