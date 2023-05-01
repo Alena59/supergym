00ac32019e2f9e03a6e9304db9b20aa9522fa817
Alena59 <asdalena59@gmail.com>
2023-05-01 03:45:56 +0300

    lint

--- source/js/modules/swiper-reviews.js
+++ source/js/modules/swiper-reviews.js
@@ -1,19 +1,21 @@
 import Swiper from '../vendor/swiper';
 const reviewsSwiper = document.querySelector('.reviews__swiper');
 
-
 const initReviewsSwiper = () => {
   if (reviewsSwiper) {
-    new Swiper('.reviews__swiper', {
-      direction: 'horizontal',
-      loop: false,
+    (() =>
+      new Swiper('.reviews__swiper', {
+        direction: 'horizontal',
+        loop: false,
 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    })
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      })
+    )();
+
   }
-}
+};
 
 export {initReviewsSwiper};
--- source/js/modules/swiper-trainers.js
+++ source/js/modules/swiper-trainers.js
@@ -3,34 +3,36 @@ const trainersSwiper = document.querySelector('.trainers__swiper');
 
 const initTrainersSwiper = () => {
   if (trainersSwiper) {
-    new Swiper('.trainers__swiper', {
-      direction: 'horizontal',
-      loop: true,
+    (() =>
+      new Swiper('.trainers__swiper', {
+        direction: 'horizontal',
+        loop: true,
 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
-      breakpoints: {
-
-        320: {
-          slidesPerView: 1,
-          // spaceBetween: 20,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
 
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 30
-        },
+        breakpoints: {
 
-        1200: {
-          slidesPerView: 4,
-          spaceBetween: 40,
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
         },
-      }
-    })
+      })
+    )();
   }
-}
+};
 
 export {initTrainersSwiper};
