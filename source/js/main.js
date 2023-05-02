671eab327b02603da8d41f4383c438b30b9cd8e5
Alena59 <asdalena59@gmail.com>
2023-05-02 12:10:47 +0300

    finish

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-// import {initModals} from './modules/modals/init-modals';
-// import {Form} from './modules/form-validate/form';
 import {initTrainersSwiper} from './modules/swiper-trainers';
 import {initReviewsSwiper} from './modules/swiper-reviews';
 import {setupVideo} from './modules/video';
@@ -13,19 +11,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
   // ---------------------------------
-  sendForm();
   iosVhFix();
 
   // Modules
   // ---------------------------------
+  sendForm();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    // initModals();
-    // const form = new Form();
-    // window.form = form;
-    // form.init();
     initTrainersSwiper();
     initReviewsSwiper();
     setupVideo();
--- source/js/modules/form-validation.js
+++ source/js/modules/form-validation.js
@@ -37,20 +37,6 @@ function validateForm(form) {
       }
     }
 
-    // if (input.dataset.validateType === 'email') {
-    //   if (validateEmail(input)) {
-    //     removeError(input);
-    //     createError(input, 'Некорректный адрес!');
-    //     result = false;
-    //   }
-    // }
-
-    // if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
-    //   removeError(input);
-    //   createError(input, 'Подтвердите согласие!');
-    //   result = false;
-    // }
-
     if (input.dataset.required === 'true') {
       if (input.value === '') {
         removeError(input);
@@ -63,10 +49,6 @@ function validateForm(form) {
   function validatePhone(input) {
     return !/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(input.value);
   }
-
-  // function validateEmail(input) {
-  //   return !/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(input.value);
-  // }
 
   return result;
 }
